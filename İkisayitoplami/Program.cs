f825aef7989d8b8d1d9806b3dbc95427019e7c5a
Mert Ramazanoglu <61631884+elistasus129@users.noreply.github.com>
2024-07-21 22:18:08 +0300

    Add project files.

---
+++ İkisayitoplami/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İkisayitoplami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1.Sayıyı Giriniz = ");
+            int sayi1=Convert.ToInt32(Console.ReadLine());
+            Console.Write("2.Sayıyı Giriniz = ");
+            int sayi2 = Convert.ToInt32(Console.ReadLine()); 
+            int sonuc=sayi1 + sayi2;
+            Console.WriteLine($"sonuc:{sonuc}");    
+
+        }
+    }
+}
