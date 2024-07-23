cf425758cac2fb897b19b8f2de7b2c59dbdd2706
Mert Ramazanoglu <61631884+elistasus129@users.noreply.github.com>
2024-07-23 23:35:05 +0300

    dsda

--- 5Doğum tarihi girilen kişinin yaşını hesaplayan program/Program.cs
+++ 5Doğum tarihi girilen kişinin yaşını hesaplayan program/Program.cs
@@ -10,6 +10,15 @@ namespace _5Doğum_tarihi_girilen_kişinin_yaşını_hesaplayan_program
     {
         static void Main(string[] args)
         {
+            {
+                int dyili, yas;
+                DateTime yil;
+                yil = DateTime.Now;
+                Console.Write("Doğum yılını giriniz = ");
+                dyili = Convert.ToInt32(Console.ReadLine());
+                yas = yil.Date.Year - dyili;
+                Console.Write("Yaş = " + yas);
+                Console.ReadLine();
+            }
         }
     }
-}
